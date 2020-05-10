02453bfeada93c66fecd9046ac08e252b1c855d7
Mihai Temian <mihai.temian@gmail.com>
2020-05-10 20:14:53 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import BackgroundImagePage from './components/Background/background.js';
+import Card from './components/Card/card.js';
+
+function App() {
+  return (
+    <BackgroundImagePage />
+  );
+}
+
+export default App;
---
+++ src/components/Background/background.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './background.css';
+
+
+const BackgroundImagePage = () => {
+  return (
+      <div className="bg"></div>
+  );
+}
+
+export default BackgroundImagePage;
