de35e7e321982411b45934789716c9e17a47cf71
Mihai Temian <mihai.temian@gmail.com>
2020-05-15 22:01:44 +0300

    api added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React from 'react';
 import './App.css';
 import BackgroundImagePage from './components/Background/background.js';
 import Card from './components/Card/card.js';
+import { fetchData } from './api/index.js';
 
-function App() {
-  return (
-    <BackgroundImagePage />
-  );
+class App extends React.Component {
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    console.log(data);
+  }
+
+  render() {
+    return (
+      <BackgroundImagePage />
+    );
+  }
 }
 
 export default App;
